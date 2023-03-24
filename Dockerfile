cdc0c2d6d50ded9223f93671b9a89c55bee28de6
MGaloto <maxigaloto@hotmail.com>
2023-03-24 13:07:25 -0300

    add new libraries

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 FROM rocker/shiny:4.2.1
 
-# Instalar paquete remotes para controlar las versiones de otros paquetes
-RUN R -e 'install.packages("remotes", repos="http://cran.rstudio.com")'
-
-# install needed R packages
-RUN R -e "install.packages('highcharter', repos='http://cran.rstudio.com/')"
 
 COPY install.R install.R
 RUN Rscript install.R
--- install.R
+++ install.R
@@ -1,3 +1,12 @@
+# Instalar paquete remotes para controlar las versiones de otros paquetes
+install.packages("remotes", repos="http://cran.rstudio.com")
+
+# install needed R packages
+install.packages('highcharter', repos='http://cran.rstudio.com/')
+
+install.packages('XML', repos='http://cran.rstudio.com/')
+install.packages('rlist', repos='http://cran.rstudio.com/')
+
 install.packages('rsconnect')
 install.packages('flexdashboard', version='0.5.2')
 install.packages('tidyverse', version='1.3.1')
@@ -7,5 +16,4 @@ install.packages('RColorBrewer', version='1.1.2')
 install.packages('httr', version='1.4.2')
 install.packages('lubridate', version='1.7.10')
 install.packages('writexl', version='1.4.0')
-
 
