5e505ef5c96667a63d9be0cfa737051758502030
MGaloto <maxigaloto@hotmail.com>
2023-03-24 14:34:02 -0300

    delete install.R

--- deploy.R
+++ deploy.R
@@ -18,7 +18,6 @@ setAccountInfo(name = error_on_missing_name("SHINY_ACC_NAME"),
 # Deploy the application.
 deployApp(appFiles = c("MacroTrends.Rmd", 
                        "downloadData.R",
-                       "install.R",
                        "www/styles.css",
                        "indecnacional.xlsx"))
 
