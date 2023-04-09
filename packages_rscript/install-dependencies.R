c47d7c14d7ec335f30f1e1569f5740770a70725e
MGaloto <maxigaloto@hotmail.com>
2023-04-09 20:49:15 -0300

    set docker and packages env 2

--- packages_rscript/install-dependencies.R
+++ packages_rscript/install-dependencies.R
@@ -1,1 +1,1 @@
-install.packages(c("highcharter", "XML", "rlist", "flexdashboard", "tidyverse", "shiny", "readxl", "RColorBrewer", "httr", "lubridate", "writexl"))
+install.packages(c("rsconnect","highcharter", "XML", "rlist", "flexdashboard", "tidyverse", "shiny", "readxl", "RColorBrewer", "httr", "lubridate", "writexl"))
